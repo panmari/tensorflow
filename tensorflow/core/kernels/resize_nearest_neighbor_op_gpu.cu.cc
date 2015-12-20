16ec4f0aa1a0eef7d686545e2bca5a89080575fb
panmari <smmuzi@gmail.com>
2015-12-20 12:25:18 +0100

    Added GPU implementation for resize nearest neighbor.
    Also added appropriate tests/benchmarks.

---
+++ tensorflow/core/kernels/resize_nearest_neighbor_op_gpu.cu.cc
@@ -0,0 +1,72 @@
+/* Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#if GOOGLE_CUDA
+
+#define EIGEN_USE_GPU
+
+#include <stdio.h>
+#include <cfloat>
+
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/util/cuda_kernel_helper.h"
+
+namespace tensorflow {
+namespace {
+
+template <typename dtype>
+__global__ void ResizeNearestNeighborNHWC(const int nthreads, const dtype* bottom_data,
+                                          const int in_height, const int in_width,
+                                          const int channels, const int out_height,
+                                          const int out_width, dtype* top_data) {
+  const float width_scale = in_width / static_cast<float>(out_width);
+  const float height_scale = in_height / static_cast<float>(out_height);
+  CUDA_1D_KERNEL_LOOP(index, nthreads) {
+    int n = index;
+    int c = n % channels;
+    n /= channels;
+    int out_x = n % out_width;
+    n /= out_width;
+    int out_y = n % out_height;
+    n /= out_height;
+
+    const dtype* bottom_data_n = bottom_data + n * channels * in_height * in_width;
+    const int in_x = min(static_cast<int>(floorf(out_x * width_scale)), in_width - 1);
+    const int in_y = min(static_cast<int>(floorf(out_y * height_scale)), in_height - 1);
+    const int idx = (in_y * in_height + in_x) * channels + c;
+    top_data[index] = bottom_data_n[idx];
+  }
+}
+
+}  // namespace
+
+bool ResizeNearestNeighbor(const float* bottom_data, const int batch,
+                           const int in_height, const int in_width,
+                           const int channels, const int out_height,
+                           const int out_width, float* top_data,
+                           const Eigen::GpuDevice& d) {
+  const int output_size = batch * channels * out_height * out_width;
+  CudaLaunchConfig config = GetCudaLaunchConfig(output_size, d);
+
+  ResizeNearestNeighborNHWC<<<config.block_count, config.thread_per_block, 0, d.stream()>>>(
+      output_size, bottom_data, in_height, in_width, channels, out_height,
+      out_width, top_data);
+  return d.ok();
+}
+
+}  // end namespace tensorflow
+
+#endif  // GOOGLE_CUDA
---
+++ tensorflow/core/kernels/resize_nearest_neighbor_op_gpu.h
@@ -0,0 +1,37 @@
+/* Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#if !GOOGLE_CUDA
+#error This file must only be included when building with Cuda support
+#endif
+
+#ifndef THIRD_PARTY_TENSORFLOW_CORE_KERNELS_RESIZE_NEAREST_NEIGHBOR_OP_GPU_H_
+#define THIRD_PARTY_TENSORFLOW_CORE_KERNELS_RESIZE_NEAREST_NEIGHBOR_OP_GPU_H_
+
+#define EIGEN_USE_GPU
+
+#include "third_party/eigen3/unsupported/Eigen/CXX11/NeuralNetworks"
+#include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/platform/port.h"
+
+namespace tensorflow {
+
+bool ResizeNearestNeighbor(const float* bottom_data, const int batch, const int in_height,
+                           const int in_width, const int channels, const int out_height,
+                           const int out_width, float* top_data, const Eigen::GpuDevice& d);
+
+}  // namespace tensorflow
+
+#endif  // THIRD_PARTY_TENSORFLOW_CORE_KERNELS_RESIZE_NEAREST_NEIGHBOR_OP_GPU_H_
